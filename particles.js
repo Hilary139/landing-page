4ec37b7b9da3cde8f5fd44f38b9cc4282ce7b293
hill <prince@gmail.com>
2020-09-20 16:26:59 +0100

    changed

---
+++ particles.js
@@ -0,0 +1,11 @@
+
+let navbar = document.querySelector(".navbar")
+let ham = document.querySelector(".ham")
+
+ham.addEventListener("click", toggleHamburger)
+
+// toggles hamburger menu in and out when clicking on the hamburger
+function toggleHamburger(){
+    navbar.classList.toggle("showNav")
+    ham.classList.toggle("showClose")
+  }
